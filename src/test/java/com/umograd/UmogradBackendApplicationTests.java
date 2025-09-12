453d70dede47b9ee5cdaef549d0d685482d2e906
LizaPoznyak <poznyak.lizochka@gmail.com>
2025-09-12 12:20:06 +0300

    Initial commit

---
+++ src/test/java/com/umograd/UmogradBackendApplicationTests.java
@@ -0,0 +1,15 @@
+package com.umograd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class UmogradBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
